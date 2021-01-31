0f4e23cf70caa08fa6251d3beedfd3bf37cda981
unknown <dzavaleta@hotmail.com>
2021-01-30 19:41:38 -0500

    feat(state): handle state using functions

--- src/contador/contador.js
+++ src/contador/contador.js
@@ -3,6 +3,10 @@ import React from 'react';
 class Contador extends React.Component {
   state = {
     clicks: 0,
+    video: {
+      title: 'Video',
+      likes: 0
+    }
   }
   // basic step
   add = () => {
@@ -10,12 +14,43 @@ class Contador extends React.Component {
       clicks: this.state.clicks + 1,
     });
   };
+  // using function to return a new state
+   add2 = () => {
+    this.setState((state, props) => {
+      return {
+        clicks: state.clicks +1
+      }
+    });
+     // just return an object using "()"
+    // this.setState((state, props) => ({
+    //     clicks: state.clicks +1
+    //   })
+    // );
+  };
+
+   addLikes = () => {
+     // just return an object using "()"
+    this.setState((state, props) => ({
+      video: {
+        ...state.video,
+          likes: state.video.likes +1
+        }
+      })
+    );
+  };
 
   render() {
     return (
+    <>
       <div>
-        <button onClick={this.add}>Clicks { this.state.clicks }</button>
+        <button onClick={this.add}>Clicks {this.state.clicks}</button>
+        <button onClick={this.add2}>Clicks with Fn { this.state.clicks }</button>
       </div>
+       <div>
+        {this.state.video.title}
+        <button onClick={this.addLikes}>Likes with Fn { this.state.video.likes }</button>
+        </div>
+    </>
     );
   }
 
