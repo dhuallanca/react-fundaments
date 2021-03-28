589a40cfd0cde130200eec1ed3666887f507b80d
unknown <dzavaleta@hotmail.com>
2021-03-28 15:36:48 -0500

    hook useState

--- src/header/header.js
+++ src/header/header.js
@@ -3,7 +3,10 @@ const Header = (props) => {
   const { title } = props;
   const divStyles = {
     border: "1px solid #E8E8E8",
+    background: "blue",
     padding: "0.5 rem",
+    color: "white",
+    width: "300px",
   };
   return (
     <>
--- src/hooks/use-state/contador.js
+++ src/hooks/use-state/contador.js
@@ -3,15 +3,41 @@ import Header from '../../header/header';
 
 const ContadorUseState = () => {
 
- const [ clicks, setClicks] = useState(0);
+  const [clicks, setClicks] = useState(0);
+  const [isActive, setActivated] = useState(false);
+  const [contador, setContador] = useState({
+    clicks: 0,
+    title: ''
+  });
 
   const addClicks = () => {
     setClicks(clicks + 1);
+  }
+  const activate = () => {
+    setActivated(!isActive);
+  }
+  const addContador = () => {
+    //only update clicks attribute, title is not sending
+    setContador({
+      clicks: contador.clicks +1
+    })
+  }
+  const addTitle = (e) => {
+    // update all the object contador
+    setContador({
+       ...contador,
+      title: e.target.value
+    })
   }
   return (
     <>
       <Header title="Use State"></Header>
       <button onClick={addClicks}>Clicks { clicks }</button>
+      <button onClick={activate}>{isActive ? 'Desactivar' : 'Activar'}</button>
+      <br></br>
+      <input onChange={addTitle}></input>
+      <button onClick={addContador}>Contador {contador.clicks}</button>
+      <h3>{contador.title}</h3>
     </>
 
   )
